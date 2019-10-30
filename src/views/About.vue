b86d925c76524f8ffea1158001e1dbe3fc633f49
Kevin Elliott <kevin@welikeinc.com>
2019-10-30 15:21:37 -0700

    Fixes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="about">
+  <div class="about container text-center">
     <h1>About</h1>
-    <div class="w-50">
-      This is an ACARS aggregation server. The data here is comprised of volunteer feeders.
-      In order to see the roadmap or submit a feature request, visit the
-      <a href="">Acars Server Public Trello Board</a>.
+    <div class="d-flex justify-content-center">
+      <div class="w-50">
+        This is an ACARS aggregation server. The data here is comprised of volunteer feeders.
+        In order to see the roadmap or submit a feature request, visit the
+        <a href="https://trello.com/b/1vq5pHNq/acars-server-public-board" target="_blank">Acars Server Public Trello Board</a>.
+      </div>
     </div>
   </div>
 </template>
