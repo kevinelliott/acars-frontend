8e783a81399df6650c1d46ab58cd3679f6a7bc32
Kevin Elliott <kevin@welikeinc.com>
2019-10-31 12:16:34 -0700

    Bump version

--- src/utilities/version.ts
+++ src/utilities/version.ts
@@ -4,7 +4,7 @@ export class Version {
   }
 
   public version() : string { // eslint-disable-line class-methods-use-this
-    return '0.1.4';
+    return '0.1.5';
   }
 
   public toString() : string { // eslint-disable-line class-methods-use-this
