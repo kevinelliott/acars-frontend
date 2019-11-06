5fa91f58c22bbb06dad48c5915b4560905029f36
Kevin Elliott <kevin@welikeinc.com>
2019-11-05 17:31:26 -0800

    Extract message decoder out to class to start centralizing it

--- src/components/MessagesListItemSlim.vue
+++ src/components/MessagesListItemSlim.vue
@@ -99,7 +99,7 @@
             <div class="text-break text-left">
               <div class="p-3 bg-white">
                 <span v-if="message.text">
-                  <div v-html="highlightKnownCodesInText(convertNewlinesToBRs(message.text))"></div>
+                  <div v-html="convertNewlinesToBRs(message.text)"></div>
                   <div v-if="decodeMessage(message)">
                     <div class="mt-1 text-muted"><small>DECODED</small></div>
                     <div v-html="decodeMessage(message)"></div>
@@ -120,6 +120,8 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
+import { MessageDecoder } from '@/utilities/decoders/acars/MessageDecoder';
+
 @Component
 export default class MessagesListItem extends Vue {
   @Prop() private message!: Object;
@@ -136,21 +138,6 @@ export default class MessagesListItem extends Vue {
     return frequencyName;
   }
 
-  highlightKnownCodesInText(text: string) : string {
-    const explodedWords : Array<string> = text.split(' ');
-
-    const highlightedWords = explodedWords.map((word) => {
-      // console.log(`Word: ${word}`);
-      const airport = this.lookupAirportByIata(word);
-      if (airport) {
-        console.log('Word Match: Airport found');
-      }
-      return word;
-    });
-
-    return highlightedWords.join(' ');
-  }
-
   labelName(label: string) : string {
     const labelObj = this.$store.state.acarsData.labels[label];
     let labelName = 'Unknown';
@@ -162,142 +149,13 @@ export default class MessagesListItem extends Vue {
     return labelName;
   }
 
-  lookupAirportByIata(iata: string) : any {
-    const airportsArray : Array<any> = this.$store.state.acarsData.airports;
-    // console.log(airportsArray);
-    const airport = airportsArray.find(e => e.iata === iata);
-
-    return airport;
-  }
-
   convertNewlinesToBRs(text: string) : string { // eslint-disable-line class-methods-use-this
     return text.replace('\n', '<br>');
   }
 
   decodeMessage(message: any) : string {
-    let decodedString = '';
-
-    if (message.label === ':;') {
-      decodedString += '<div>Aircraft Transceiver Frequency Change</div>';
-      const frequency = Number(message.text) / 1000;
-      decodedString += `${frequency} MHz`;
-    }
-
-    if (message.label === 'H1') {
-      if (message.text.includes('#M1BPOS')) {
-        console.log('DECODER: #M1BPOS detected');
-        const parts = message.text.replace('#M1BPOS', '').split('/')[0].split(',');
-        // console.log(parts);
-
-        const coordsRegex = /(?<lac>[NS])(?<la>\d+)(?<lnc>[EW])(?<ln>\d+)/;
-        const results = parts[0].match(coordsRegex);
-        // console.log(results);
-
-        const latitude = results.groups.la / 1000;
-        const longitude = results.groups.ln / 1000;
-        let route = parts.slice(1).filter((part: any) => !/^\d(.+)$/.test(part));
-        route = route.map((hop: any) => hop || '?');
-
-        decodedString += '<div>Position Report</div>';
-        decodedString += `<div>Coordinates: ${latitude} ${results.groups.lac}, ${longitude} ${results.groups.lnc}</div>`;
-        decodedString += `<div>Route: ${route.join(' > ')}</div>`;
-      }
-
-      if (message.text.includes('#M1BPRG')) {
-        console.log('DECODER: #M1BPRG detected');
-        const parts = message.text.split('/');
-        for (const part of parts) { // eslint-disable-line no-restricted-syntax
-          if (part.includes('#M')) {
-            const regex = /#M(?<fms>\w+)PRG/;
-          }
-
-          if (part.includes('DT')) {
-            const regex = /DT(?<dest>\w+),(?<rway>.+),(?<fuel>.+),(?<eta>.+),(?<rem>.+)/;
-            const result = message.text.match(regex);
-            // console.log('DT result');
-            // console.log(result);
-          }
-
-          if (part.includes('FN')) {
-            const regex = /FN(?<flight>\w+)/;
-          }
-        }
-      }
-    }
-
-    if (message.label === 'SQ') {
-      const preamble = message.text.substring(0, 4);
-      const version = message.text[1];
-      let network = this.$store.state.acarsData.labels.SQ.decoderHints.brands[message.text[3]];
-      let airport;
-      let iataCode;
-      let icaoCode;
-      let latitude;
-      let longitude;
-      let stationNumber;
-      let vdlFrequency;
-
-      if (!network) {
-        network = 'Unknown';
-      }
-
-      if (preamble === '02XA') {
-        const regex = /0(\d)X(?<org>\w)(?<iata>\w\w\w)(?<icao>\w\w\w\w)(?<station>\d)(?<lat>\d+)(?<latd>[NS])(?<lng>\d+)(?<lngd>[EW])V(?<vfreq>\d+)\/ARINC/;
-        const result = message.text.match(regex);
-
-        if (result.length >= 8) {
-          iataCode = result.groups.iata;
-          icaoCode = result.groups.icao;
-          stationNumber = result.groups.station;
-          airport = this.lookupAirportByIata(iataCode);
-          latitude = `${Number(result.groups.lat) / 1000} ${result.groups.latd}`;
-          longitude = `${Number(result.groups.lng) / 1000} ${result.groups.lngd}`;
-          vdlFrequency = result.groups.vfreq;
-        }
-      }
-
-      const decodedMessage = {
-        description: 'Ground Station Squitter',
-        preamble,
-        version,
-        network,
-        iataCode,
-        icaoCode,
-        stationNumber,
-        airport,
-        latitude,
-        longitude,
-        vdlFrequency,
-      };
-      console.log(decodedMessage);
-
-      decodedString = `
-        <div>${decodedMessage.description}</div>
-        <div>Network: ${decodedMessage.network}</div>
-      `;
-
-      if (decodedMessage.stationNumber) {
-        decodedString += `<div>Ground Station: ${decodedMessage.icaoCode}${decodedMessage.stationNumber}</div>`;
-      }
-
-      if (iataCode) {
-        decodedString += `<div>IATA: ${iataCode}</div>`;
-      }
-
-      if (icaoCode) {
-        decodedString += `<div>ICAO: ${icaoCode}</div>`;
-      }
-
-      if (decodedMessage.airport) {
-        decodedString += `<div>Airport: ${decodedMessage.airport.name} (${decodedMessage.airport.icao}) in ${decodedMessage.airport.location}</div>`;
-      }
-
-      if (decodedMessage.latitude) {
-        decodedString += `<div>Coordinates: ${decodedMessage.latitude}, ${decodedMessage.longitude}`;
-      }
-    }
-
-    return decodedString;
+    const decoder = new MessageDecoder(this.$store);
+    return decoder.decodeMessage(message);
   }
 }
 </script>
---
+++ src/utilities/decoders/acars/MessageDecoder.ts
@@ -0,0 +1,146 @@
+import { Store } from 'vuex';
+
+export class MessageDecoder {
+  store : Store<any>;
+
+  constructor(store: Store<any>) {
+    this.store = store;
+  }
+
+  decodeMessage(message: any) : string {
+    let decodedString = '';
+
+    if (message.label === ':;') {
+      decodedString += '<div>Aircraft Transceiver Frequency Change</div>';
+      const frequency = Number(message.text) / 1000;
+      decodedString += `${frequency} MHz`;
+    }
+
+    if (message.label === 'H1') {
+      if (message.text.includes('#M1BPOS')) {
+        console.log('DECODER: #M1BPOS detected');
+        const parts = message.text.replace('#M1BPOS', '').split('/')[0].split(',');
+        // console.log(parts);
+
+        const coordsRegex = /(?<lac>[NS])(?<la>\d+)(?<lnc>[EW])(?<ln>\d+)/;
+        const results = parts[0].match(coordsRegex);
+        // console.log(results);
+
+        const latitude = results.groups.la / 1000;
+        const longitude = results.groups.ln / 1000;
+        let route = parts.slice(1).filter((part: any) => !/^\d(.+)$/.test(part));
+        route = route.map((hop: any) => hop || '?');
+
+        decodedString += '<div>Position Report</div>';
+        decodedString += `<div>Coordinates: ${latitude} ${results.groups.lac}, ${longitude} ${results.groups.lnc}</div>`;
+        decodedString += `<div>Route: ${route.join(' > ')}</div>`;
+      }
+
+      if (message.text.includes('#M1BPRG')) {
+        console.log('DECODER: #M1BPRG detected');
+        const parts = message.text.split('/');
+        for (const part of parts) { // eslint-disable-line no-restricted-syntax
+          if (part.includes('#M')) {
+            const regex = /#M(?<fms>\w+)PRG/;
+          }
+
+          if (part.includes('DT')) {
+            const regex = /DT(?<dest>\w+),(?<rway>.+),(?<fuel>.+),(?<eta>.+),(?<rem>.+)/;
+            const result = message.text.match(regex);
+            // console.log('DT result');
+            // console.log(result);
+          }
+
+          if (part.includes('FN')) {
+            const regex = /FN(?<flight>\w+)/;
+          }
+        }
+      }
+    }
+
+    if (message.label === 'SQ') {
+      const preamble = message.text.substring(0, 4);
+      const version = message.text[1];
+      let network = this.store.state.acarsData.labels.SQ.decoderHints.brands[message.text[3]];
+      let airport;
+      let iataCode;
+      let icaoCode;
+      let latitude;
+      let longitude;
+      let stationNumber;
+      let vdlFrequency;
+
+      if (!network) {
+        network = 'Unknown';
+      }
+
+      if (preamble === '02XA') {
+        const regex = /0(\d)X(?<org>\w)(?<iata>\w\w\w)(?<icao>\w\w\w\w)(?<station>\d)(?<lat>\d+)(?<latd>[NS])(?<lng>\d+)(?<lngd>[EW])V(?<vfreq>\d+)\/ARINC/;
+        const result = message.text.match(regex);
+
+        if (result.length >= 8) {
+          iataCode = result.groups.iata;
+          icaoCode = result.groups.icao;
+          stationNumber = result.groups.station;
+          airport = this.lookupAirportByIata(iataCode);
+          latitude = `${Number(result.groups.lat) / 1000} ${result.groups.latd}`;
+          longitude = `${Number(result.groups.lng) / 1000} ${result.groups.lngd}`;
+          vdlFrequency = result.groups.vfreq;
+        }
+      }
+
+      const decodedMessage = {
+        description: 'Ground Station Squitter',
+        preamble,
+        version,
+        network,
+        iataCode,
+        icaoCode,
+        stationNumber,
+        airport,
+        latitude,
+        longitude,
+        vdlFrequency,
+      };
+      console.log(decodedMessage);
+
+      decodedString = `
+        <div>${decodedMessage.description}</div>
+        <div>Network: ${decodedMessage.network}</div>
+      `;
+
+      if (decodedMessage.stationNumber) {
+        decodedString += `<div>Ground Station: ${decodedMessage.icaoCode}${decodedMessage.stationNumber}</div>`;
+      }
+
+      if (iataCode) {
+        decodedString += `<div>IATA: ${iataCode}</div>`;
+      }
+
+      if (icaoCode) {
+        decodedString += `<div>ICAO: ${icaoCode}</div>`;
+      }
+
+      if (decodedMessage.airport) {
+        decodedString += `<div>Airport: ${decodedMessage.airport.name} (${decodedMessage.airport.icao}) in ${decodedMessage.airport.location}</div>`;
+      }
+
+      if (decodedMessage.latitude) {
+        decodedString += `<div>Coordinates: ${decodedMessage.latitude}, ${decodedMessage.longitude}`;
+      }
+    }
+
+    return decodedString;
+  }
+
+  lookupAirportByIata(iata: string) : any {
+    const airportsArray : Array<any> = this.store.state.acarsData.airports;
+    // console.log(airportsArray);
+    const airport = airportsArray.find(e => e.iata === iata);
+
+    return airport;
+  }
+}
+
+export default {
+};
