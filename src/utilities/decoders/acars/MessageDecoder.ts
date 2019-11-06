4965ff28f2cfbf226c8a862cec1c71a3215444d9
Kevin Elliott <kevin@welikeinc.com>
2019-11-06 00:50:33 -0800

    Decoder enhancement

--- src/utilities/decoders/acars/MessageDecoder.ts
+++ src/utilities/decoders/acars/MessageDecoder.ts
@@ -16,24 +16,152 @@ export class MessageDecoder {
       decodedString += `${frequency} MHz`;
     }
 
+    if (message.label === '15') {
+      // General Aviation Position Report
+      decodedString += '<div>Position Report</div>';
+
+      const twoZeeRegex = /^\(2(?<between>.+)\(Z$/;
+      const results = message.text.match(twoZeeRegex);
+      if (results) {
+        // Style: (2N38111W 82211266 76400-64(Z
+        console.log(`Label 15 Position Report: between = ${results.groups.between}`);
+      }
+    }
+
+    if (message.label === '5Z') {
+      decodedString += '<div>Airline Designated Downlink</div>';
+
+      const uaRegex = /^\/(?<type>\w+) (?<remainder>.+)/;
+      let results = message.text.match(uaRegex);
+
+      if (results && results.length >= 2) {
+        // Successful match: United Airlines 5Z message
+        const type = results.groups.type.split('/')[0];
+        const { remainder } = results.groups;
+        console.log(results);
+        console.log(`DECODER: Matched 'United Airlines 5Z': type = ${type}, remainder = ${remainder}`);
+
+        let typeDescription;
+        switch (type) {
+          case 'B1':
+            typeDescription = 'Request Weight and Balance';
+            break;
+          case 'B3':
+            typeDescription = 'Request Departure Clearance';
+            break;
+          case 'CD':
+            typeDescription = 'Weight and Balance';
+            break;
+          case 'CG':
+            typeDescription = 'Request Pre-departure clearance, PDC';
+            break;
+          case 'CM':
+            typeDescription = 'Crew Scheduling';
+            break;
+          case 'C3':
+            typeDescription = 'Off Message';
+            break;
+          case 'C4':
+            typeDescription = 'Flight Dispatch';
+            break;
+          case 'C5':
+            typeDescription = 'Maintenance Message';
+            break;
+          case 'C6':
+            typeDescription = 'Customer Service';
+            break;
+          case '10':
+            typeDescription = 'PIREP';
+            break;
+          case 'C11':
+            typeDescription = 'International PIREP';
+            break;
+          case 'DS':
+            typeDescription = 'Late Message';
+            break;
+          case 'D3':
+            typeDescription = 'Holding Pattern Message';
+            break;
+          case 'D6':
+            typeDescription = 'From-To + Date';
+            break;
+          case 'D7':
+            typeDescription = 'From-To + Alternate + Time';
+            break;
+          case 'EO':
+            typeDescription = 'In Range';
+            break;
+          case 'PW':
+            typeDescription = 'Position Weather';
+            break;
+          case 'RL':
+            typeDescription = 'Request Release';
+            break;
+          case 'R3':
+            typeDescription = 'Request HOWGOZIT Message';
+            break;
+          case 'R4':
+            typeDescription = 'Request the Latest POSBD';
+            break;
+          case 'TC':
+            typeDescription = 'From-To Fuel';
+            break;
+          case 'WB':
+            typeDescription = 'From-To';
+            break;
+          case 'W1':
+            typeDescription = 'Request Weather for City';
+            break;
+          default:
+            typeDescription = 'Unknown';
+        }
+
+        decodedString += '<div>Airline: United Airlines</div>';
+        decodedString += `<div>Message Type: ${typeDescription} (${type})</div>`;
+
+        if (type === 'B3') {
+          const rdcRegex = /^(?<from>\w\w\w)(?<to>\w\w\w) (?<unknown1>\d\d) R(?<runway>.+) G(?<unknown2>.+)$/;
+          results = remainder.match(rdcRegex);
+
+          if (results) {
+            decodedString += `<div>Origin: ${results.groups.from}</div>`;
+            decodedString += `<div>Destination: ${results.groups.to}</div>`;
+            decodedString += `<div>Unknown Field 1: ${results.groups.unknown1}</div>`;
+            decodedString += `<div>Runway: ${results.groups.runway}</div>`;
+            decodedString += `<div>Unknown FIeld 2: ${results.groups.unknown2}</div>`;
+          } else {
+            console.log(`Unkown 5Z RDC format: ${remainder}`);
+          }
+        } else {
+          decodedString += `<div>Remainder: ${remainder} (Will analyze and decode in the future)</div>`;
+        }
+      } else {
+        // Unknown
+        console.log(`Unknown 5Z message: ${message.text}`);
+      }
+    }
+
     if (message.label === 'H1') {
       if (message.text.includes('#M1BPOS')) {
         console.log('DECODER: #M1BPOS detected');
         const parts = message.text.replace('#M1BPOS', '').split('/')[0].split(',');
         // console.log(parts);
 
+        decodedString += '<div>Position Report</div>';
+
         const coordsRegex = /(?<lac>[NS])(?<la>\d+)(?<lnc>[EW])(?<ln>\d+)/;
         const results = parts[0].match(coordsRegex);
         // console.log(results);
 
-        const latitude = results.groups.la / 1000;
-        const longitude = results.groups.ln / 1000;
-        let route = parts.slice(1).filter((part: any) => !/^\d(.+)$/.test(part));
-        route = route.map((hop: any) => hop || '?');
+        if (results && results.length >= 4) {
+          const latitude = results.groups.la / 1000;
+          const longitude = results.groups.ln / 1000;
+          let route = parts.slice(1).filter((part: any) => !/^\d(.+)$/.test(part));
+          route = route.map((hop: any) => hop || '?');
 
-        decodedString += '<div>Position Report</div>';
-        decodedString += `<div>Coordinates: ${latitude} ${results.groups.lac}, ${longitude} ${results.groups.lnc}</div>`;
-        decodedString += `<div>Route: ${route.join(' > ')}</div>`;
+          decodedString += `<div>Coordinates: ${latitude} ${results.groups.lac}, ${longitude} ${results.groups.lnc}</div>`;
+          decodedString += `<div>Route: ${route.join(' > ')}</div>`;
+        }
       }
 
       if (message.text.includes('#M1BPRG')) {
@@ -58,6 +186,50 @@ export class MessageDecoder {
       }
     }
 
+    if (message.label === 'SA') {
+      // Media Report
+      // Example: 0EV021358VSH/
+      // 0      = Version Number
+      // E      = Establishment/Loss flag (1)
+      // V      = Media Identification (2)
+      // 021358 = UTC Timestamp
+      // V      = Current Media Available
+      // S      = Current Media Available
+      // H      = Current Media Available
+      // /      = Delimiter (May contain free text after the delimiter)
+      //
+      // (1) Flags:
+      // E = Media Established
+      // L = Media Lost
+      //
+      // (2) Media Types:
+      // V = VHF-ACARS
+      // S = Default Satcom
+      // H = HF
+      // G = Global Star Satcom
+      // C = ICO Satcom
+      // 2 = VDL Mod 2
+      // X = Inmarsat Aero H/H+/I/L
+      // I = Iridium Satcom
+      const parts = message.text.split('/');
+      const version = parts[0].text[0];
+      const linkState = parts[0].text[1];
+      const mediaId = parts[0].text[2];
+      const timestamp = parts[0].text.substr(3, 8);
+      const availableMedia = parts[0].text.substr(8).split('');
+      const freeText = parts[1];
+
+      decodedString += '<div>Media Report</div>';
+      decodedString += `<div>Version: ${version}</div>`;
+      decodedString += `<div>Link State: ${linkState}</div>`;
+      decodedString += `<div>Media Identification: ${mediaId}</div>`;
+      decodedString += `<div>Timestamp: ${timestamp.substr(0, 2)}:${timestamp.substr(2, 4)}:${timestamp.substr(4, 6)} UTC</div>`;
+      decodedString += `<div>Available Media: ${availableMedia.join(', ')}</div>`;
+      if (freeText) {
+        decodedString += `<div>Free Text: ${freeText}</div>`;
+      }
+    }
+
     if (message.label === 'SQ') {
       const preamble = message.text.substring(0, 4);
       const version = message.text[1];
@@ -74,8 +246,8 @@ export class MessageDecoder {
         network = 'Unknown';
       }
 
-      if (preamble === '02XA') {
-        const regex = /0(\d)X(?<org>\w)(?<iata>\w\w\w)(?<icao>\w\w\w\w)(?<station>\d)(?<lat>\d+)(?<latd>[NS])(?<lng>\d+)(?<lngd>[EW])V(?<vfreq>\d+)\/ARINC/;
+      if (version === '2') {
+        const regex = /0(\d)X(?<org>\w)(?<iata>\w\w\w)(?<icao>\w\w\w\w)(?<station>\d)(?<lat>\d+)(?<latd>[NS])(?<lng>\d+)(?<lngd>[EW])V(?<vfreq>\d+)\/.*/;
         const result = message.text.match(regex);
 
         if (result.length >= 8) {
