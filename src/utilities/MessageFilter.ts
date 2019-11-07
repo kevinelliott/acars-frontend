9a338d6d124e5025477ed31d63a4955ccd791a80
Kevin Elliott <kevin@welikeinc.com>
2019-11-06 17:57:19 -0800

    Extract message filtering out to a reusable class with chain style

---
+++ src/utilities/MessageFilter.ts
@@ -0,0 +1,52 @@
+export class MessageFilter {
+  messages : Array<any> = [];
+
+  constructor(messages: Array<any>) {
+    this.messages = messages;
+  }
+
+  excludeByErrors(errorsToExclude: Array<string>) : MessageFilter {
+    this.messages = this.messages.filter(message => !errorsToExclude.includes(message.error));
+    return this;
+  }
+
+  excludeByLabels(labelsToExclude: Array<string>) : MessageFilter {
+    this.messages = this.messages.filter(message => !labelsToExclude.includes(message.label));
+    return this;
+  }
+
+  includeByLabels(labelsToInclude: Array<string>) : MessageFilter {
+    if (labelsToInclude.length > 0) {
+      this.messages = this.messages.filter(message => labelsToInclude.includes(message.label));
+    }
+    return this;
+  }
+
+  includeByAirframes(airframeIdsToInclude: Array<string>) : MessageFilter {
+    if (airframeIdsToInclude.length > 0) {
+      this.messages = this.messages.filter((message: any) => airframeIdsToInclude.includes(message.airframe.id)); // eslint-disable-line max-len
+    }
+    return this;
+  }
+
+  includeByStations(stationsIdsToInclude: Array<string>) : MessageFilter {
+    if (stationsIdsToInclude.length > 0) {
+      this.messages = this.messages.filter((message: any) => stationsIdsToInclude.includes(message.station.id)); // eslint-disable-line max-len
+    }
+    return this;
+  }
+
+  includeByText(textToInclude: string) : MessageFilter {
+    if (textToInclude && textToInclude !== '') {
+      this.messages = this.messages.filter(message => message.text.includes(textToInclude));
+    }
+    return this;
+  }
+
+  filter(): Array<any> {
+    return this.messages;
+  }
+}
+
+export default {
+};
